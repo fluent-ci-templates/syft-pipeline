11957070210c8c14ee0b2a9719b35c2b74f467a9
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-08 15:59:52 +0000

    feat: add wasm plugin

---
+++ plugin/src/lib.rs
@@ -0,0 +1,12 @@
+use extism_pdk::*;
+use fluentci_pdk::dag;
+
+#[plugin_fn]
+pub fn sbom(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pkgx()?
+        .with_packages(vec!["syft"])?
+        .with_exec(vec!["syft", "scan", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
