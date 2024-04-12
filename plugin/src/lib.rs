6160c22f43f4f848580f8dbf3cd81b271719f4a7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-04-12 04:59:53 +0000

    [plugin] update functions

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -2,6 +2,16 @@ use extism_pdk::*;
 use fluentci_pdk::dag;
 
 #[plugin_fn]
+pub fn scan(args: String) -> FnResult<String> {
+    let stdout = dag()
+        .pkgx()?
+        .with_packages(vec!["syft"])?
+        .with_exec(vec!["syft", "scan", &args])?
+        .stdout()?;
+    Ok(stdout)
+}
+
+#[plugin_fn]
 pub fn sbom(args: String) -> FnResult<String> {
     let stdout = dag()
         .pkgx()?
